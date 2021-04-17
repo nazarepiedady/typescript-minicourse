c7f501ed7783788f1a061e36201baafd9924218c
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-17 19:32:37 +0100

    01: define a sum function to sum two values

--- 01-first-type-code/index.js
+++ 01-first-type-code/index.js
@@ -1,3 +1,8 @@
 const $firstInput = document.getElementById('first-number');
 const $secondInput = document.getElementById('second-number');
 const $sumButton = document.getElementById('sum-button');
+
+function sum(a, b) {
+  return a + b;
+}
+
