f8248f7c3ea5bfed6ce5351ff21d7b27443a447e
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-17 20:48:24 +0100

    02: define an array variables

--- 03-types/types.ts
+++ 03-types/types.ts
@@ -9,3 +9,10 @@ message = `foo ${isOpen}`
 // number (int, float, hexdecimal, binary)
 let total: number
 total = 0xff2
+
+// array (type[] / Array<type>)
+let items: string[]
+items = ["foo", "bar"]
+
+let values: Array<number>
+values = [1, 2, 3]
