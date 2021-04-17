162da017dc3e22bc592d52db7bdd14f956c60fd8
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-17 20:24:31 +0100

    02: delete index.js on the root

--- 02-create-tsconfig/index.js
+++
@@ -1,9 +0,0 @@
-var $firstInput = document.getElementById('first-number');
-var $secondInput = document.getElementById('second-number');
-var $sumButton = document.getElementById('sum-button');
-function sum(a, b) {
-    return a + b;
-}
-$sumButton.addEventListener('click', function () {
-    console.log(sum(Number($firstInput.value), Number($secondInput.value)));
-});
