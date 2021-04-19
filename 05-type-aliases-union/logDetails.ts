ae15762697dcf50a9e65fe8940a529df6b45bc70
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-19 19:20:48 +0100

    05: use Platform to type the argument of renderPlatform

--- 05-type-aliases-union/logDetails.ts
+++ 05-type-aliases-union/logDetails.ts
@@ -12,5 +12,9 @@ function logInfo(uid: Uid, user: string) {
 // predefine type alias with values
 type Platform = 'Windows' | 'Linux' | 'Mac OS'
 
+function renderPlatform(platform: Platform) {
+  return  `<h1>${platform}</h1>`
+}
+
 logDetails(123, 'sapato');
 logDetails('123', 'sapato');
