758fb9f1e4f04940715188d03aa77629a3eba246
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-19 19:11:40 +0100

    05: define a logInfo with same caracteristic of logDetails

--- 05-type-aliases-union/logDetails.ts
+++ 05-type-aliases-union/logDetails.ts
@@ -2,5 +2,9 @@ function logDetails(uid: number | string, item: string) {
   console.log(`A product with ${uid} has a title as ${item}.`);
 }
 
+function logInfo(uid: number | string, user: string) {
+  console.log(`An user with ${uid} has a name as ${user}.`);
+}
+
 logDetails(123, 'sapato');
 logDetails('123', 'sapato');
